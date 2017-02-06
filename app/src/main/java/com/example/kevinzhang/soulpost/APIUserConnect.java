e0c08e344a901cfd40748feab62901249837253b
Jonathan Loewen <jmloewen@sfu.ca>
2017-02-05 17:45:36 -0800

    User Registration and Update functionality moved to APIUserConnect static class, Maps are now properly overlaid.  Working on lat/lng update for Devices.

---
+++ app/src/main/java/com/example/kevinzhang/soulpost/APIUserConnect.java
@@ -0,0 +1,17 @@
+package com.example.kevinzhang.soulpost;
+
+/**
+ * Created by Jonathan on 2/5/2017.
+ */
+
+public class APIUserConnect {
+    /***
+     * This is for first time user connection, creates a registration with a location-device id tuple.
+     */
+    public static void RegisterDevice(){
+
+    }
+    public static void UpdateDevice(){
+
+    }
+}
