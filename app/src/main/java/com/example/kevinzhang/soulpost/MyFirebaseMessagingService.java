ef7786b1fbdf097e85c30b372155f28448afa5f5
Kai Zhang <kai@d207-023-222-131.wireless.sfu.ca>
2017-02-26 16:56:59 -0800

    soul catched from s3 upon receiving notification

--- app/src/main/java/com/example/kevinzhang/soulpost/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/kevinzhang/soulpost/MyFirebaseMessagingService.java
@@ -2,9 +2,19 @@ package com.example.kevinzhang.soulpost;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.google.firebase.auth.api.model.StringList;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.File;
 
 /**
  * Created by kevinzhang on 2016-12-17.
@@ -32,5 +42,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         editor.putString("PushS3Key", remoteMessage.getData().get("S3key"));
         editor.commit();
         Log.d(TAG, "S3Key: " + prefs.getString("PushS3Key", "NO KEY STORED"));
+        beginDownload(prefs.getString("PushS3Key", "NO KEY STORED"));
+
+
+    }
+
+    private void beginDownload(final String S3key){
+        Log.d(TAG, "Download has begun");
+
+        TransferUtility mTransferUtility;
+        mTransferUtility = Util.getTransferUtility(this);
+        File audioFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),
+                String.valueOf(S3key));
+
+        TransferObserver observer = mTransferUtility.download(Constants.BUCKET_NAME, S3key, audioFile);
+
     }
 }
