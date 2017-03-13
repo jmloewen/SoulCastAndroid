375907c75dcd5b62a6d5d128b7e1339b9ea62d00
jla497 <jaehonglee86@gmail.com>
2017-03-12 18:35:09 -0700

    added Kevin's changes

--- app/src/main/java/com/example/kevinzhang/soulpost/Soul.java
+++ app/src/main/java/com/example/kevinzhang/soulpost/Soul.java
@@ -5,21 +5,32 @@ package com.example.kevinzhang.soulpost;
  */
 
 public class Soul {
-    String soulType;
-    String s3Key;
-    int epoch;
-    double longitude;
-    double latitude;
-    double radius;
-    String token;
+    private String soulType;
+    private String s3Key;
+    private long epoch;
+    private double longitude;
+    private double latitude;
+    private double radius;
+    private String token;
+    private Device userDevice;
 
-    public Soul(String soulType, String s3Key, int epoch, double longitude, double latitude, double radius, String token) {
+    public Soul(String soulType, String s3Key, long epoch, Device userDevice) {
+        this.userDevice = userDevice;
         this.soulType = soulType;
         this.s3Key = s3Key;
         this.epoch = epoch;
-        this.longitude = longitude;
-        this.latitude = latitude;
-        this.radius = radius;
-        this.token = token;
+        this.latitude = userDevice.getLatitude();
+        this.longitude = userDevice.getLongitude();
+        this.radius = userDevice.getRadius();
+        this.token = userDevice.getToken();
     }
+
+    public void setSoulType(String soulType){this.soulType = soulType;}
+    public void setS3Key(String s3Key){this.s3Key = s3Key;}
+    public void setEpoch(long epoch){this.epoch = epoch;}
+    public void setUserDevice(Device userDevice){this.userDevice = userDevice;}
+    public String getSoulType(){return soulType;}
+    public String getS3Key(){return s3Key;}
+    public long getEpoch(){return epoch;}
+    public Device getUserDevice(){return userDevice;}
 }
